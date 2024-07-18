b5aa4ca03aa43fadbb4cf06406432b0246c4bc40
Colin <colinclissonhg@gmail.com>
2024-07-18 18:59:10 -0400

    feat: add new chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,64 @@
 <script setup lang="ts">
-import { add, format } from "date-fns";
+import { add, format, sub } from "date-fns";
 
-const dataSample = [8421, 12802, 6234, 7321, 8543, 5463, 11382, 9382, 12342, 8234];
-const prioritySample = [12, 11, 16, 18, 12, 19, 17, 11, 14, 15];
-const arrivalsByDay = new Array(10)
-  .fill(0)
-  .map((_, index) => ({
-    x: format(add(new Date(), { days: index }), "E, dd MMM"),
-    y: dataSample[index],
-  }));
-const priorityArrivalsByDay = new Array(10)
-  .fill(0)
-  .map((_, index) => ({
-    x: "High priority",
-    y: Math.floor(dataSample[index] * prioritySample[index] / 100),
-  }));
+const generateArrivals = () => {
+  const dataSample = [8421, 12802, 6234, 7321, 8543, 5463, 11382, 9382, 12342, 8234];
+  const prioritySample = [12, 11, 16, 18, 12, 19, 17, 11, 14, 15];
+  const arrivalsByDay = new Array(10)
+    .fill(0)
+    .map((_, index) => ({
+      x: format(add(new Date(), { days: index }), "E, dd MMM"),
+      y: dataSample[index],
+    }));
+  const priorityArrivalsByDay = new Array(10)
+    .fill(0)
+    .map((_, index) => ({
+      x: "High priority",
+      y: Math.floor(dataSample[index] * prioritySample[index] / 100),
+    }));
+
+  return [
+    {
+      name: "Arrivals",
+      data: arrivalsByDay,
+    },
+    {
+      name: "High priority",
+      data: priorityArrivalsByDay,
+    },
+  ];
+};
+
+const generateAlgorithmPerformance = () => {
+  const dataSample = [1252, 1211, 1633, 1867, 1528, 1939, 1732, 1111, 1234, 1435];
+  const falsePositives = [22, 13, 31, 22, 14, 26, 38, 31, 28, 19];
+  const containersPerDay = new Array(10)
+    .fill(0)
+    .map((_, index) => ({
+      x: format(sub(new Date(), { days: index }), "E, dd MMM"),
+      y: dataSample[index],
+    }))
+    .reverse();
+  const falsePositivesPerDay = new Array(10)
+    .fill(0)
+    .map((_, index) => ({
+      x: "False positives",
+      y: falsePositives[index],
+    }))
+    .reverse();
+
+  return [
+    {
+      name: "AI detected high priority containers",
+      data: containersPerDay,
+    },
+    {
+      name: "False positives",
+      data: falsePositivesPerDay,
+    },
+  ];
+};
+
 const generateTruckTraffic = () => {
   const days = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"].reverse();
   const hours = ["8:00", "9:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"];
@@ -52,20 +96,76 @@ const generateTruckTraffic = () => {
                   type: 'bar',
                   stacked: true,
                 },
-                series: [
-                  {
-                    name: 'Arrivals',
-                    data: arrivalsByDay
-                  },
-                  {
-                    name: 'High priority',
-                    data: priorityArrivalsByDay
-                  }
-                ],
-                colors: ['#008ffb', '#ef4444'],
+                series: generateArrivals(),
+                colors: ['#3b81f4', '#ed4343'],
                 dataLabels: {
                   enabled: false
                 },
+              }"
+            />
+          </Card>
+          <Card class="col-span-6">
+            <div class="flex justify-between">
+              <p class="text-lg font-medium mb-4">
+                Algorithm false positives
+              </p>
+              <DateRangerPicker />
+            </div>
+            <ApexChart
+              :options="{
+                chart: {
+                  type: 'bar',
+                  stacked: true,
+                },
+                series: generateAlgorithmPerformance(),
+                colors: ['#22C55E', '#ed4343'],
+                dataLabels: {
+                  enabled: false
+                },
+              }"
+            />
+          </Card>
+        </div>
+        <div class="grid grid-cols-12 gap-4">
+          <Card class="col-span-6">
+            <div class="flex justify-between">
+              <p class="text-lg font-medium mb-4">
+                Priority containers by supply type
+              </p>
+              <DateRangerPicker />
+            </div>
+            <ApexChart
+              class="flex justify-center"
+              :options="{
+                chart: {
+                  type: 'donut',
+                  width: '80%',
+                },
+                series: [2344, 1255, 813],
+                colors: ['#3b81f4', '#a754f5', '#f23f5d'],
+                labels: ['Medical Supplies', 'Medical Equipment', 'Pharmaceuticals'],
+                plotOptions: {
+                  pie: {
+                    donut: {
+                      size: '50%',
+                      labels: {
+                        show: true,
+                        total: {
+                          show: true,
+                          showAlways: true,
+                          label: 'Total',
+                        },
+                      }
+                    }
+                  }
+                },
+                legend: {
+                  show: true,
+                  position: 'bottom'
+                },
+                dataLabels: {
+                  enabled: false
+                }
               }"
             />
           </Card>
@@ -111,47 +211,6 @@ const generateTruckTraffic = () => {
                 legend: {
                   show: false,
                 },
-              }"
-            />
-          </Card>
-        </div>
-        <div class="grid grid-cols-12 gap-4">
-          <Card class="col-span-6">
-            <div class="flex justify-between">
-              <p class="text-lg font-medium mb-4">
-                Priority containers by supply type
-              </p>
-              <DateRangerPicker />
-            </div>
-            <ApexChart
-              :options="{
-                chart: {
-                  type: 'donut'
-                },
-                series: [2344, 1255, 813],
-                labels: ['Medical Supplies', 'Medical Equipment', 'Pharmaceuticals'],
-                plotOptions: {
-                  pie: {
-                    donut: {
-                      size: '50%',
-                      labels: {
-                        show: true,
-                        total: {
-                          show: true,
-                          showAlways: true,
-                          label: 'Total',
-                        },
-                      }
-                    }
-                  }
-                },
-                legend: {
-                  show: true,
-                  position: 'bottom'
-                },
-                dataLabels: {
-                  enabled: false
-                }
               }"
             />
           </Card>
